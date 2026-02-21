26757e1ce34249e428398282aff99cf121e48016
Tech5818 <pqow5818@gmail.com>
2026-02-21 17:52:53 +0900

    add to Profile features

---
+++ src/features/toProfile/ui/index.tsx
@@ -0,0 +1,7 @@
+import { DetailProfile } from "@shared/ui/DetailProfile";
+
+export const ToProfileInfo = () => {
+  return (
+    <DetailProfile name="윤지상" phone="010-1111-1111" sendGift={2} memo={13} />
+  );
+};
