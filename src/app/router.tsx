91d44cea05dbe59ef33d8934e0daf8bad9a9cd92
Tech5818 <pqow5818@gmail.com>
2026-02-21 14:59:47 +0900

    init

---
+++ src/app/router.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "@pages/home";
+import "./index.css";
+
+export const RouterProvider = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Home</h1>
+    </div>
+  );
+};
