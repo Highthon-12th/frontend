b0ebb90eac532fd16b0cd0f39c12dd9333dd1d89
Tech5818 <pqow5818@gmail.com>
2026-02-21 19:28:39 +0900

    shadow change

--- src/widgets/bottomButton/ui/index.tsx
+++ src/widgets/bottomButton/ui/index.tsx
@@ -8,7 +8,7 @@ export const BottomButtonWidgets = ({
   text = "",
 }: ButtonButtonWidgetsProps) => {
   return (
-    <div className="w-full bg-white px-5 py-3">
+    <div className="w-full bg-white px-5 py-3 shadow-[0_-4px_20px_0_rgba(0,0,0,0.04)">
       <Button text={text} className="w-full" />
     </div>
   );
--- src/widgets/header/ui/index.tsx
+++ src/widgets/header/ui/index.tsx
@@ -2,7 +2,7 @@ import Bell from "@shared/img/bell.svg?react";
 
 export const HeaderWidghets = () => {
   return (
-    <header className="bg-white px-5 py-3 flex justify-between items-center shadow-[0_0_20px_0_rgba(0,0,0,0.04)]">
+    <header className="bg-white px-5 py-3 pt-12.5 flex justify-between items-center shadow-[0_4px_20px_0_rgba(0,0,0,0.04)]">
       <p className="text-base text-main font-bold">로고</p>
       <Bell />
     </header>
